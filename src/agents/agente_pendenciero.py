caf6bbaf0349e1303bca1e1f8155d18a46def70b
Eric <eric.tornas@gmail.com>
2024-04-20 21:01:00 -0500

    .

--- src/agents/agente_pendenciero.py
+++ src/agents/agente_pendenciero.py
@@ -3,7 +3,43 @@ from environment.sim_object import Object_Info
 from environment.actions import Action_Info, Action, Attack
 from Interfaces.IAgent import IAgent
 from random import randint
+from ai.search.MCTS import MCTS
+class Pendenciero(IAgent):
+    def pendencieroF():
+        pass
+    def move(self, possible_destinations: List[Tuple[int, int]]) -> Tuple[int, int]:
+        return possible_destinations[randint(0, len(possible_destinations) - 1)]
+    
+    def inform_move(self, position: Tuple[int]) -> None:
+        pass
 
-from typing import Tuple, List
-from environment.actions import Action, MoveAction, AttackAction
-from environment.sim_object import Object_Info
+    def get_actions(self, possible_victims : List[int]) -> List[Action]:
+        if possible_victims:
+            selected_victim = possible_victims[randint(0, len(possible_victims) - 1)]
+            return [Attack(None, selected_victim, 1)]
+        return []
+
+    def inform_of_attack_made(self, victim_id: int, strength: int) -> None:
+        print("He realizado un ataque a " + str(victim_id) + " con fuerza " + str(strength))
+
+    def inform_of_received_attack(self, attacker_id: int, strength: int) -> None:
+        print("He recibido un ataque de " + str(attacker_id) + " con fuerza " + str(strength))
+
+    def take_attack_reward(self, victim_id: int, reward: int):
+        print("He cobrado una recompensa de " + str(reward) + " por matar a " + str(victim_id))
+
+    def see_objects(self, info: List[Object_Info]) -> None:
+        for obj in info:
+            print(str(obj.position) + " : " + str(obj.id))
+        pass
+
+    def see_resources(self, info: List[Tuple[Tuple[int, int], int]]) -> None:
+        pass
+
+    def see_actions(self, info : List[Action_Info]):
+        for action in info:
+            print(str(action.type) + " in " + str(action.start_position))
+
+    def feed(self, sugar: int) -> None:
+        pass
+
